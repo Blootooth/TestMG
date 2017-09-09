143a466e4002dd88004514cc315c26d7e2a1e18f
nu1silva <nu1silva@gmail.com>
2017-09-09 18:30:47 +0530

    updated the whole testmg structure

--- com.nu1silva.testmg.util/src/main/java/com/nu1silva/testmg/util/auth/PasswordUtils.java
+++ com.nu1silva.testmg.util/src/main/java/com/nu1silva/testmg/util/auth/PasswordUtils.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.nu1silva.testmg.util;
+package com.nu1silva.testmg.util.auth;
 
 import org.jasypt.util.password.ConfigurablePasswordEncryptor;
 
